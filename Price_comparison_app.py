b00bbc6cd13d0d8822415b69ef0c988131fbd4ea
Abhijeet Kr <94948306+itsabhijeet80@users.noreply.github.com>
2024-11-05 23:43:52 +0530

    Update Price_comparison_app.py
    Deepsource ai review bot

--- Price_comparison_app.py
+++ Price_comparison_app.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from bs4 import BeautifulSoup
 import requests
-
+#this is review for deepsource ai pr bot
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.34'}
 
