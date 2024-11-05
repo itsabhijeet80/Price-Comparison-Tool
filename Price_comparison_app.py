cc0d39aca2ab3ebdec3dfc09ab2a5f29fac5a0a3
Abhijeet Kr <94948306+itsabhijeet80@users.noreply.github.com>
2024-11-06 00:03:10 +0530

    Update Price_comparison_app.py
    again test

--- Price_comparison_app.py
+++ Price_comparison_app.py
@@ -13,6 +13,9 @@ x=5
 y=8
 
 
+ds=ada
+
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.34'}
 
